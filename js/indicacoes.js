fc444a1852ebb033bc4376072c42967ba7ff6863
Henry Adam <iihenymii@gmail.com>
2025-06-12 19:47:57 -0300

    Pagina indicacoes aba fitness adicionada

--- js/indicacoes.js
+++ js/indicacoes.js
@@ -7,3 +7,24 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+const abas = document.querySelectorAll('.aba-categoria')
+const secaoFitness = document.querySelector('.secao-cartoes.fitness')
+const secaoReceitas = document.querySelector('.secao-cartoes.receitas')
+
+abas.forEach(aba => {
+    aba.addEventListener('click', () => {
+        abas.forEach(a => a.classList.remove('ativa'))
+        aba.classList.add('ativa')
+
+        const categoriaSelecionada = aba.dataset.categoria
+
+        if (categoriaSelecionada === 'fitness') {
+            secaoFitness.classList.remove('hidden')
+            secaoReceitas.classList.add('hidden')
+        } else {
+            secaoReceitas.classList.remove('hidden')
+            secaoFitness.classList.add('hidden')
+        }
+    })
+})
